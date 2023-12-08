f317fcdffd3bf0a38066ca2b774cf082dc583a62
zhiyizhang <zhang.zhiyi3@northeastern.edu>
2023-12-07 19:40:49 -0800

    check_funds and faud

--- routers/check_funds_and_fraud.py
+++ routers/check_funds_and_fraud.py
@@ -3,22 +3,17 @@ from pydantic import BaseModel
 from db.database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
-from models.models import Account, Transaction
+from models.models import Account, CreditCard, DebitCard
 
 router = APIRouter()
 
 
 class FundAndFraudCheckRequest(BaseModel):
-    transaction_id: int
     account_id: int
     amount: float
-
-
-class FundAndFraudCheckResponse(BaseModel):
-    transaction_id: int
-    account_id: int
-    amount: float
-    status: str
+    card_id: int
+    card_type: str
+    user_id: int
 
 
 # Fetch database
@@ -34,28 +29,33 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@router.post("/check_funds_and_fraud", response_model=FundAndFraudCheckResponse, status_code=201)
+@router.post("/check_funds_and_fraud", status_code=201)
 def check_funds_and_fraud_endpoint(db: db_dependency, request: FundAndFraudCheckRequest):
-    if not request.transaction_id or not request.account_id or request.amount or request.amount <= 0:
-        raise HTTPException(status_code=400, detail="Invalid Request")
+    if not request.user_id or not request.card_id or not request.card_type or \
+            not request.account_id or not request.amount or request.amount <= 0:
+        raise HTTPException(status_code=400, detail="Invalid Request, missing request parameters")
 
-    transaction_id, account_id = request.transaction_id, request.account_id
-    transaction = db.query(Transaction).filter(Transaction.id == transaction_id)
-    account = db.query(Account).filter(Account.id == account_id)
-    amount = transaction.amount
-    balance = account.balance
-    if check_fraud(amount) is False or check_funds(amount, balance) is False:
-        new_status = "Rejected"
-        transaction.status = new_status
-    db.refresh(transaction)
-    return transaction
+    user_id, card_id, account_id, card_type, amount = request.user_id, request.card_id, request.account_id, request.card_type, request.amount
+    account_user_id = db.query(Account.user_id).filter(Account.id == account_id).first()[0]
 
+    if account_user_id != user_id:
+        raise HTTPException(status_code=400, detail="Account doesn't belong to user")
 
-# Check if the card provided has enough balance
-def check_funds(amount, balance):
-    if amount > balance:
-        return False
-    return True
+    if card_type == "Credit":
+        card_user = db.query(CreditCard.user_id).filter(CreditCard.id == card_id).first()
+        if card_user != user_id:
+            raise HTTPException(status_code=400, detail="Card doesn't belong to user")
+    else:
+        card_user = db.query(DebitCard.user_id).filter(CreditCard.id == card_id).first()
+        if card_user != user_id:
+            raise HTTPException(status_code=400, detail="Card doesn't belong to user")
+        balance = db.query(Account.balance).filter(Account.id == account_id).first()
+        if balance < amount:
+            raise HTTPException(status_code=400, detail="Insufficient fund")
+
+    if check_fraud(amount) is False:
+        raise HTTPException(status_code=400, detail="Amount exceeding limits")
+    return {"Check result": "pass"}
 
 
 # Check if this transaction may be fraud
